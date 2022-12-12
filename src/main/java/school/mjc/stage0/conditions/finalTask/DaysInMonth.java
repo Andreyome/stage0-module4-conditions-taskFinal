1dafae8c50e2ecadc458f7512ba472138cb7e8a0
Andreyome <andreyomem@gmail.com>
2022-12-12 15:22:06 +0200

    fix 1

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,37 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        if(month >0 && month <13) {
+            if (month != 2) {
+                if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+                    System.out.println(31);
+                } else {
+                    System.out.println(30);
+                }
+            }
+            else {
+                if (year > 0) {
+                    if (year % 4 == 0) {
+                        if (year % 100 == 0) {
+                            if (year % 400 == 0) {
+                                System.out.println(29);
+                            } else{
+                                System.out.println(28);}
+                        } else{
+                            System.out.println(29);}}
+                        else{
+                            System.out.println(28);
+                        }
+                    }
+                else{
+                    System.out.println("invalid date");
+                }
+                }
+            }
+
+        else{
+                System.out.println("invalid date");
+            }
 
     }
 }
